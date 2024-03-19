a26ebd2716c131670209f1085c5d642d2829fc94
walter <belwalterv@gmail.com>
2024-03-19 18:52:32 -0300

    se agrega file libreria

---
+++ mi_libreria.py
@@ -0,0 +1,28 @@
+
+def suma(n1, n2):
+    """ """
+    return n1 + n2
+
+
+def resta(n1, n2):
+    """ la docu """
+    return n1 - n2
+
+
+def producto(n1, n2):
+    """
+    Calculate the product of two numbers.
+
+    :param n1: The base number
+    :type n1: int
+    :param n2: The exponent number
+    :type n2: int
+    :returns: The result of base raised to the power of exponent.
+    :rtype: int
+    """
+    return n1 * n2
+
+
+def division(n1, n2):
+    """ """
+    return n1 / n2
