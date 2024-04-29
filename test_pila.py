68c403cfaaaefd2689e275528de4be228349e82c
walter <belwalterv@gmail.com>
2024-04-29 20:37:10 -0300

    pila con datas compuestos

--- test_pila.py
+++ test_pila.py
@@ -19,3 +19,27 @@ while pila_aux.size() > 0:
 
 print()
 print(pila.size())
+
+
+class Peli:
+
+    def __init__(self, nombre, anio, traje):
+        self.nombre = nombre
+        self.anio = anio
+        self.traje = traje
+
+    def __str__(self):
+        return f'{self.nombre} - {self.anio} - {self.traje}'
+
+
+peliculas = [
+    Peli('avengers', 2000, 'Mark V'),
+    Peli('avengers Era de Ultron', 2006, 'Mark VIII'),
+]
+
+
+for pelicula in peliculas:
+    pila_aux.push(pelicula)
+
+while pila_aux.size() > 0:
+    print(pila_aux.pop().traje)
