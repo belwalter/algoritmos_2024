6f36246a0ca93d9de2f39d93592876cc12f3be1c
belwalter <belwalterv@gmail.com>
2024-08-13 19:52:11 -0300

    add barridos

--- arbol.py
+++ arbol.py
@@ -15,24 +15,79 @@ class BinaryTree:
     def insert_node(self, value):
         def __insert(root, value):
             if root is None:
-                print('crear nodo')
                 return BinaryTree.__Node(value)
             elif value < root.value:
-                print('insertar izquierda')
                 root.left = __insert(root.left, value)
             else:
-                print('insertar derecha')
                 root.right = __insert(root.right, value)
             return root
 
         self.root = __insert(self.root, value)
 
+    def search(self, key):
+        def __search(root, key):
+            if root is not None:
+                if root.value == key:
+                    # print('lo encontre')
+                    return root
+                elif key < root.value:
+                    # print(f'buscalo a la izquierda de {root.value}')
+                    return __search(root.left, key)
+                else:
+                    # print(f'buscalo a la derecha de {root.value}')
+                    return __search(root.right, key)
+            # else:
+            #     print('no hay nada')
+        aux = None
+        if self.root is not None:
+            aux = __search(self.root, key)
+        return aux
 
+    def preorden(self):
+        def __preorden(root):
+            if root is not None:
+                print(root.value)
+                # a = input()
+                # print(f'izquierda de {root.value}')
+                __preorden(root.left)
+                # print(f'derecha de {root.value}')
+                __preorden(root.right)
+
+        if self.root is not None:
+            __preorden(self.root)
+
+    def inorden(self):
+        def __inorden(root):
+            if root is not None:
+                __inorden(root.left)
+                print(root.value)
+                __inorden(root.right)
+
+        if self.root is not None:
+            __inorden(self.root)
+
+    def postorden(self):
+        def __postorden(root):
+            if root is not None:
+                __postorden(root.right)
+                print(root.value)
+                __postorden(root.left)
+
+        if self.root is not None:
+            __postorden(self.root)
 
 tree = BinaryTree()
 tree.insert_node(19)
 tree.insert_node(7)
 tree.insert_node(31)
 tree.insert_node(11)
+tree.insert_node(45)
+tree.insert_node(22)
+tree.insert_node(27)
 
-print(tree.root.value, tree.root.left, tree.root.right)
+pos = tree.search(27)
+if pos:
+    print('lo encontre', pos.value)
+else:
+    print('no esta')
+
