e5c1e036574c2e492d3e88babf908aa89b771feb
belwalter <belwalterv@gmail.com>
2024-08-26 19:28:09 -0300

    prueba arbol

--- arbol.py
+++ arbol.py
@@ -1,28 +1,29 @@
 
-
+from cola import Queue
 
 class BinaryTree:
 
     class __Node:
-        def __init__(self, value, left=None, right=None):
+        def __init__(self, value, left=None, right=None, other_value=None):
             self.value = value
             self.left = left
             self.right = right
+            self.other_value = other_value
 
     def __init__(self):
         self.root = None
 
-    def insert_node(self, value):
-        def __insert(root, value):
+    def insert_node(self, value, other_value=None):
+        def __insert(root, value, other_value=None):
             if root is None:
-                return BinaryTree.__Node(value)
+                return BinaryTree.__Node(value, other_value=other_value)
             elif value < root.value:
-                root.left = __insert(root.left, value)
+                root.left = __insert(root.left, value, other_value)
             else:
-                root.right = __insert(root.right, value)
+                root.right = __insert(root.right, value, other_value)
             return root
 
-        self.root = __insert(self.root, value)
+        self.root = __insert(self.root, value, other_value)
 
     def search(self, key):
         def __search(root, key):
@@ -47,7 +48,6 @@ class BinaryTree:
         def __preorden(root):
             if root is not None:
                 print(root.value)
-                # a = input()
                 # print(f'izquierda de {root.value}')
                 __preorden(root.left)
                 # print(f'derecha de {root.value}')
@@ -55,6 +55,18 @@ class BinaryTree:
 
         if self.root is not None:
             __preorden(self.root)
+
+    def contar_super_heroes(self):
+        def __contar_super_heroes(root):
+            counter = 0
+            if root is not None:
+                if root.other_value.get('is_hero') is True:
+                    counter = 1
+                counter += __contar_super_heroes(root.left)
+                counter += __contar_super_heroes(root.right)
+            return counter
+
+        return __contar_super_heroes(self.root)
 
     def inorden(self):
         def __inorden(root):
@@ -76,13 +88,48 @@ class BinaryTree:
         if self.root is not None:
             __postorden(self.root)
 
+    def inorden_villanos(self):
+        def __inorden_villanos(root):
+            if root is not None:
+                __inorden_villanos(root.left)
+                if root.other_value.get('is_hero') is not True:
+                    print(root.value)
+                __inorden_villanos(root.right)
+
+        if self.root is not None:
+            __inorden_villanos(self.root)
+
+    def inorden_superheros_start_with(self, start):
+        def __inorden_superheros_start_with(root, start):
+            if root is not None:
+                __inorden_superheros_start_with(root.left, start)
+                if root.other_value.get('is_hero') is True and root.value.startswith(start):
+                    print(root.value)
+                __inorden_superheros_start_with(root.right, start)
+
+        if self.root is not None:
+            __inorden_superheros_start_with(self.root, start)
+
+    def by_level(self):
+        pendientes = Queue()
+        if self.root is not None:
+            pendientes.arrive(self.root)
+
+        while pendientes.size() > 0:
+            node = pendientes.attention()
+            print(node.value)
+            if node.left is not None:
+                pendientes.arrive(node.left)
+            if node.right is not None:
+                pendientes.arrive(node.right)
+
     def delete_node(self, value):
         def __replace(root):
             if root.right is None:
-                print(f'no tiene derecha es el mayor {root.value}')
+                # print(f'no tiene derecha es el mayor {root.value}')
                 return root.left, root
             else:
-                print('seguir buscando nodo par remplazar a la dercha')
+                # print('seguir buscando nodo par remplaz+ar a la dercha')
                 root.right, replace_node = __replace(root.right)
                 return root, replace_node
 
@@ -117,16 +164,16 @@ class BinaryTree:
             self.root, delete_value = __delete(self.root, value)
         return delete_value
 
-tree = BinaryTree()
+# tree = BinaryTree()
 
-tree.insert_node(19)
-tree.insert_node(7)
-tree.insert_node(31)
-tree.insert_node(11)
-tree.insert_node(10)
-tree.insert_node(45)
-tree.insert_node(22)
-tree.insert_node(27)
+# tree.insert_node(19)
+# tree.insert_node(7)
+# tree.insert_node(31)
+# tree.insert_node(11)
+# tree.insert_node(10)
+# tree.insert_node(45)
+# tree.insert_node(22)
+# tree.insert_node(27)
 
 # pos = tree.search(27)
 # if pos:
@@ -134,9 +181,9 @@ tree.insert_node(27)
 # else:
 #     print('no esta')
 
-tree.delete_node(7)
-tree.delete_node(11)
-tree.delete_node(31)
+# tree.delete_node(7)
+# tree.delete_node(11)
+# tree.delete_node(31)
 # tree.delete_node(27)
 # tree.delete_node(45)
 # tree.delete_node(22)
@@ -144,5 +191,9 @@ tree.delete_node(31)
 # tree.delete_node(10)
 # tree.insert_node(27)
 
-print(tree.delete_node(100))
-tree.preorden()
+# print(tree.delete_node(100))
+# tree.preorden()
+
+# print(tree.root.right)
+
+# tree.by_level()
